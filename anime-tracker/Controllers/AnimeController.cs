6c982ed9e6a83bffd45ee607178402ba6fc46910
Anthony <anthonykfho@gmail.com>
2023-02-19 18:07:33 -0500

    add create new anime function; work on update/delete

--- anime-tracker/Controllers/AnimeController.cs
+++ anime-tracker/Controllers/AnimeController.cs
@@ -6,19 +6,28 @@ using System.Web.Mvc;
 using System.Net.Http;
 using System.Diagnostics;
 using anime_tracker.Models;
+using System.Web.Script.Serialization;
 
 namespace anime_tracker.Controllers
 {
     public class AnimeController : Controller
     {
+        private static readonly HttpClient client;
+        JavaScriptSerializer jss = new JavaScriptSerializer();
+
+
+        static AnimeController()
+        {
+            client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:44383/api/animedata/");
+        }
         // GET: Anime/List
         public ActionResult List()
         {
             //Objective: communicate with our anime data API to retrieve a list of anime
             //curl https://localhost:44383/api/animedata/listanimes
            
-            HttpClient client = new HttpClient () { };
-            string url = "https://localhost:44383/api/animedata/listanimes";
+            string url = "listanimes";
             HttpResponseMessage response = client.GetAsync(url).Result;
 
             Debug.WriteLine("The response code is ");
@@ -38,8 +47,7 @@ namespace anime_tracker.Controllers
             //Objective: communicate with our anime data API to retrieve a specific anime
             //curl https://localhost:44383/api/animedata/findanime/{id}
 
-            HttpClient client = new HttpClient() { };
-            string url = "https://localhost:44383/api/animedata/findanime/" + id;
+            string url = "findanime/" + id;
             HttpResponseMessage response = client.GetAsync(url).Result;
 
             Debug.WriteLine("The response code is ");
@@ -52,25 +60,44 @@ namespace anime_tracker.Controllers
             return View(selectedAnime);
         }
 
-        // GET: Anime/Create
-        public ActionResult Create()
+        public ActionResult Error()
+        {
+            return View();
+        }
+
+        // GET: Anime/New
+        public ActionResult New()
         {
             return View();
         }
 
         // POST: Anime/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public ActionResult Create(Anime anime)
         {
-            try
-            {
-                // TODO: Add insert logic here
+            Debug.WriteLine("The inputted anime name is: ");
+            Debug.WriteLine(anime.anime_title);
+            //Objective: add a new anime into our system using the API
+            //curl -H "Content-Type:application/json" -d anime.json https://localhost:44383/api/animedata/addanime
+            string url = "addanime";
 
-                return RedirectToAction("Index");
-            }
-            catch
+            string jsonpayload = jss.Serialize(anime);
+
+            Debug.WriteLine(jsonpayload);
+
+            HttpContent content = new StringContent(jsonpayload);
+            content.Headers.ContentType.MediaType = "application/json";
+
+            HttpResponseMessage response = client.PostAsync(url, content).Result;
+
+            if(response.IsSuccessStatusCode)
             {
-                return View();
+                return RedirectToAction("List");
+
+            }
+            else
+            {
+                return RedirectToAction("Error");
             }
         }
 
